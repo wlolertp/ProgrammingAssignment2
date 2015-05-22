37842304c2012a179fcf0075c4b078b63b9d563c
wlolertp <wannal.gm@gmail.com>
2015-05-22 17:44:27 -0400

    Implement makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# The makeCacheMatrix is a function that stores 
+# a list of get, set, getInverse, and setInverse functions
 makeCacheMatrix <- function(x = matrix()) {
-
+   # initialize im.val variable that stores inverse of square matrix "x" to null
+   im.val <- NULL
+   # set value of matrix
+   set <- function(y) {
+      # Substitute the matrix x with y 
+      x <<- y
+      # reset inverse value stored in im.val variable to NULL
+      im.val <<- NULL
+   }
+   # get value of matrix
+   get <- function() x
+   # set value of inverse
+   setInverse <- function(inverse.val) im.val <<- inverse.val
+   # get value of inverse
+   getInverse <- function() im.val
+   # return list of functions
+   list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve function calculates inverse of matrix created with makeCacheMatrix function. 
+# However it first checks to see if the inverse has been calculated. 
+# If so, it gets inverse from the cache and skip computation.
+# Otherwise, it calculates inverse of matrix using solve function 
+# and sets value of the inverse in the cache via "setInverse" function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   # get inverse from cache
+   im.val <- x$getInverse()
+   # If inverse has been calculated, get cached data and return 
+   if (!is.null(im.val)){ 
+      message("getting cached data")
+      # return inverse from cache
+      return(im.val)
+   }
+   
+   # get matrix to be computed from cache and put value in mat variable
+   mat <- x$get()
+   ## check if matrix has an inverse 
+   # determinant must not be zero if it has an inverse
+   det.val <- tryCatch(det(mat), error = function(e) { 
+                           print(e) # print error
+                           return (NULL) # return NULL 
+                     })
+   # if it is not square matrix OR determinant is null, stop 
+   if (nrow(mat) != ncol(mat) | is.null(det.val)) {
+      stop(paste(nrow(mat), "x", ncol(mat), " matrix is not invertible."))
+   } 
+   # compute inversion of matrix
+   im.val <- solve(mat) 
+   # call setInverse function to cache inverse value
+   x$setInverse(im.val)
+   ## Return a matrix that is the inverse of "mat"
+   im.val 
 }
